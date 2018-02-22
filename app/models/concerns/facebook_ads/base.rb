e9294e0e853f62c90ea4b877ee8f5f88c43ae9a7
Swati Aggarwal <swati.aggarwal2412@gmail.com>
2018-02-22 18:34:05 +0530

    base added

--- app/models/concerns/facebook_ads/base.rb
+++ app/models/concerns/facebook_ads/base.rb
@@ -1,11 +1,13 @@
 class FacebookAds::Base
 
+  ACCOUNT_ID = '24237132'
+
   def post
     Rest::Client.post(url, query)
   end
 
   def payload
-
+    { "access_token": "EAASZAlYctZB1ABAAlnZC1GZCLkPzRv1H0pz5yyC9QZBZBRfctDYB7EFUzvHSCbSWPDAtwZCFmGmomuQyhi9Ja2Vj3xog6ZCKpH3dmj8Qguv2QsD5esrEbmJz6bWZCWuR02qwIJmgZB9zel5zWzwMmsypc8FBob4hoWGXvOgeSMX7QdrQZDZD" }
   end
 
   def action
